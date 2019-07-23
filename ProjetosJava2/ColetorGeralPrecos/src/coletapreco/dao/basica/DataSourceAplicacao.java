e2bb13e3eadc47610a44b29565f5694bbd4be137
tr626987 <tr626987@oi.corp.net>
2019-07-23 10:45:05 -0300

    tentando mysql stf

--- ProjetosJava2/ColetorGeralPrecos/src/coletapreco/dao/basica/DataSourceAplicacao.java
+++ ProjetosJava2/ColetorGeralPrecos/src/coletapreco/dao/basica/DataSourceAplicacao.java
@@ -4,7 +4,7 @@ import br.com.digicom.lib.dao.MySqlDataSource;
 
 public class DataSourceAplicacao extends MySqlDataSource {
 	public DataSourceAplicacao() {
-		super("jdbc:mysql://localhost/coletorgeralbd", "coletorgeral", "aspnet");
-		//super("jdbc:mysql://DESENV-PC/coletorgeralbd", "coletor", "coletor");
+		//super("jdbc:mysql://localhost/coletorgeralbd", "coletorgeral", "aspnet");
+		super("jdbc:mysql://localhost/coletorgeralbd", "root", "root");
 	}
 }
