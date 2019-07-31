142d770185eaa3ba008ef0843a364eeb3f6230db
tr626987 <tr626987@oi.corp.net>
2019-07-31 11:29:40 -0300

    temos proxy

--- ProjetosJava2/TesteConectorMaven/coletor/src/main/java/br/com/digicom/parse/ExecutadorParseApache2.java
+++ ProjetosJava2/TesteConectorMaven/coletor/src/main/java/br/com/digicom/parse/ExecutadorParseApache2.java
@@ -5,30 +5,30 @@ import java.io.Reader;
 import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-import java.util.Base64;
 
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.text.html.parser.ParserDelegator;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.xml.sax.SAXException;
 
 import br.com.digicom.lib.dao.DaoException;
 import br.com.digicom.parse.callback.ICallbackParse;
 import br.com.digicom.parse.callback.IDadosParse;
+import br.com.digicom.parse.log.ArquivoLog;
 
-public class ExecutadorParseApache {
+public class ExecutadorParseApache2 {
 
 	private ICallbackParse callbackParse;
 	private IDadosParse dadosParse;
@@ -46,21 +46,16 @@ public class ExecutadorParseApache {
 		do {
 			main();
 		} while (this.callbackParse.getLoop());
-
+		
 	}
-
+	
 	public void main() throws DaoException {
-		// CloseableHttpClient httpclient = HttpClients.createDefault();
+		//CloseableHttpClient httpclient = HttpClients.createDefault();
 		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
-		//this.setProxy(httpClientBuilder);
 		CloseableHttpClient httpclient = httpClientBuilder.build();
 		try {
 			HttpGet httpget = new HttpGet(this.callbackParse.getUrl().toURI());
 			System.out.println("Executing request " + httpget.getRequestLine());
-			// Base64.Encoder enc= Base64.getEncoder();
-			// byte[] strenc =enc.encode("tr626987:Piquet08".getBytes("UTF-8"));
-			// httpget.addHeader("Authorization", "Basic "+ strenc);
-
 			// Create a custom response handler
 			ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
 
@@ -78,7 +73,7 @@ public class ExecutadorParseApache {
 			};
 			String responseBody = httpclient.execute(httpget, responseHandler);
 			Reader reader = new StringReader(responseBody);
-			// System.out.println(responseBody);
+			//System.out.println(responseBody);
 			this.callbackParse.inicializacao();
 			new ParserDelegator().parse(reader, (HTMLEditorKit.ParserCallback) this.callbackParse, true);
 			this.callbackParse.finalizacaoOk();
@@ -101,15 +96,6 @@ public class ExecutadorParseApache {
 		}
 	}
 
-	private void setProxy(HttpClientBuilder httpClientBuilder) {
-		// Tratamento de Proxy
-		HttpHost proxy = new HttpHost("10.21.7.10", 82);
-		httpClientBuilder.setProxy(proxy);
-		CredentialsProvider credProvider = new BasicCredentialsProvider();
-		credProvider.setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT),
-				new UsernamePasswordCredentials("tr626987", "Piquet08"));
-		httpClientBuilder.setDefaultCredentialsProvider(credProvider);
-		// ---------------------------------------------------------
-	}
 
+	
 }
