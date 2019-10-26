d84ee733f3f5855ea98e5486ce9182c207f1d8e7
Paulo Alexandre <paulofore@gmail.com>
2019-10-26 11:46:07 -0200

    versao quase ok com oportunidades para o cosmetic center

--- ProjetosJava2/ColetorGeralPrecos/src/coletapreco/desen/ColetaCosmeticoApp.java
+++ ProjetosJava2/ColetorGeralPrecos/src/coletapreco/desen/ColetaCosmeticoApp.java
@@ -4,20 +4,22 @@ import br.com.digicom.lib.dao.DaoException;
 import coletapreco.regracolecao.ContagemProdutoRegraColecao;
 import coletapreco.regracolecao.FabricaRegra;
 import coletapreco.regracolecao.LojaVirtualRegraColecao;
+import coletapreco.thread.ProcessaOportunidadeTh;
 
 public class ColetaCosmeticoApp {
 
 	public static void main(String[] args) {
-		System.out.println("Versao: 31-08-2019 (3)");
+		System.out.println("Versao: 25-10-2019 (1)");
 		
 		LojaVirtualRegraColecao srv = FabricaRegra.getInstancia().getLojaVirtualRegraColecao();
 		ContagemProdutoRegraColecao contagemSrv = FabricaRegra.getInstancia().getContagemProdutoRegraColecao();
+		ProcessaOportunidadeTh oportunidade = new ProcessaOportunidadeTh();
 		
 		try {
 			
 			srv.AtualizaCosmetico();
 			contagemSrv.RegistraQuantidadesDia();
-			
+			oportunidade.run();
 			
 			
 		} catch (DaoException e) {
